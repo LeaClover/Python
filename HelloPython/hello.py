8a1abf312840447ead40582c6c525a7c4445a02a
LeaClover <plesen_14@mail.ru>
2022-09-05 14:46:59 +0300

    add project HelloPython

---
+++ HelloPython/hello.py
@@ -0,0 +1,18 @@
+print('hello python')
+
+value = None
+a = 123
+b = 1.23
+print(type(a))
+print(type(b))
+value = 123
+
+s = 'hello world'
+print(s)
+
+print('{} - {} - {}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+# print('{1} - {2} - {0}.format(a, b, s))
+
+list = [1, 2, 3]
+print(list)
