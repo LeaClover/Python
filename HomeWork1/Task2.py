25ce2297529175404f1b8332120a8f62729bc3c9
LeaClover <plesen_14@mail.ru>
2022-09-07 13:53:50 +0300

    new editting

--- HomeWork1/Task2.py
+++ HomeWork1/Task2.py
@@ -16,7 +16,7 @@ z = int(input('Введите значение z: '))
 check(x, y, z)
 
 
-# or x in range(2):
+# for x in range(2):
 #     for y in range(2):
 #         for z in range(2):
 #             print((not(x or y or z)) == (not(x) and not(y) and not(z)))
