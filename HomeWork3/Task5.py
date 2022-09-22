6ef3f243eae7a1d19fbe374ab8d918dbed7503c4
LeaClover <plesen_14@mail.ru>
2022-09-22 16:27:47 +0300

    editting hw3 task5

--- HomeWork3/Task5.py
+++ HomeWork3/Task5.py
@@ -42,3 +42,17 @@ def print_negafib(n):
 n = int(input('Введите количество чисел НегаФибоначчи, которое надо вывести: '))
 print('Последовательность НегаФибоначчи: ')
 print_negafib(n)
+
+# # НегаФибоначчи
+
+# list_1 = list()
+# a0 = 0
+# a1 = 1
+# n = int(input())
+# for i in range(n + 1):
+#     list_1.append(a0)
+#     x = a0 + a1
+#     a0 = a1
+#     a1 = x
+# list_2 = [list_1[i] * (-1) ** (i + 1) for i in range(len(list_1))][::-1]
+# print(list_2 + list_1[1:])
