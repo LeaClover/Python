42c2508c24833482aa99b6923681fb4cf2a66695
LeaClover <plesen_14@mail.ru>
2022-09-23 20:58:47 +0300

    add new project hw6 task1

---
+++ HomeWork6/Task1.py
@@ -0,0 +1,10 @@
+# Мимикрия
+
+values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+transformation = lambda x: x
+
+transformed_values = list(map(transformation, values))
+if values == transformed_values:
+    print('Равны')
+else:
+    print('Не равны!')
