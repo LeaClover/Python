ff21c56d89b60efbee6b75ec97e665e5dbcbbe54
LeaClover <plesen_14@mail.ru>
2022-09-27 00:54:17 +0300

    editting hw6 task3

--- HomeWork6/Task3.py
+++ HomeWork6/Task3.py
@@ -19,3 +19,19 @@ def check_rifm(poem):
 
 poem = input('Введите стихотворение: ')
 check_rifm(poem)
+
+
+# Второй вариант
+
+# text = input().split()
+# set_1 = set()
+# for i in text:
+#     count = 0
+#     for j in i:
+#         if j.lower() in 'еёоуяаюэи':
+#             count += 1
+#     set_1.add(count)
+# if len(set_1) == 1:
+#     print('yes')
+# else:
+#     print('no')
