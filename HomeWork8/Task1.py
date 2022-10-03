fc1e756f959a47df98cd40d280eeb8282724dd7a
LeaClover <plesen_14@mail.ru>
2022-10-03 15:59:30 +0300

    add new project hw8 task1

---
+++ HomeWork8/Task1.py
@@ -0,0 +1,23 @@
+﻿# Хакер Василий получил доступ к классному журналу и хочет заменить 
+# все свои минимальные оценки на максимальные. Напишите программу, 
+# которая заменяет оценки Василия, но наоборот: все максимальные – на минимальные.
+
+# Input: 5 -> 1 3 3 3 4
+# Output: 1 3 3 3 1
+import random
+
+def fill_list(n, list):
+    for i in range(n):
+        list.append(random.randint(1, 5))
+    return list
+
+def change_mark(list_of_mark):
+    for i in range(len(list_of_mark)):
+        if list_of_mark[i] >= 4:
+            list_of_mark[i] = random.randint(1, 3)
+    print(list_of_mark)
+
+n = int(input('Введите количесво оценок: '))
+list_of_mark = []
+print(fill_list(n, list_of_mark))
+change_mark(list_of_mark)
